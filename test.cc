d4395dee3c6c7e3b1a63a216b85c2076c4e53003
JacobM233 <JacobMijares@csu.fullerton.edu>
2026-03-12 19:49:12 +0000

    done

--- test.cc
+++ test.cc
@@ -1,2 +1,22 @@
 // Jacob Mijares
 // 838781508
+
+#include <string>
+#include <iostream>
+
+void double_int(int& x) {
+    x = x * 2;
+    std::cout << "x is " << x << "\n";
+}
+
+void double_int_pointer(int* x) {
+    *(x + 1) = *(x + 1) * 2;
+    std::cout << "x is " << *x << "\n";
+}
+
+int main () {
+    int value []= {5, 7, 15, 17, 19};
+    std::cout << "Value is " << value[0] << "\n";
+    double_int_pointer(value);
+    std::cout << "Value is " << *value << "\n";
+}
